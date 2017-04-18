39fb16849878a4b28c9076572a64cd62becbf8f1
SCIENCE-PC\kiryu <naonaokiryu2@gmail.com>
2017-04-18 17:27:02 +0900

    Changed copyright notification

--- ICSSdroid/ICSSdroid.NativeActivity/include/graphics/gles/GLVBO.h
+++ ICSSdroid/ICSSdroid.NativeActivity/include/graphics/gles/GLVBO.h
@@ -1,7 +1,7 @@
 /*
 (c) 2016,2017 Grain
 
-This file is part of ICSEdit.
+This file is part of ICSSdroid.
 
 ICSEdit is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
